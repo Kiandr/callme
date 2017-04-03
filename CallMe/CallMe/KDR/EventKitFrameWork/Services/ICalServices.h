8a065536c0f51f52e8830d9353329749b161fb3a
Kian Davoudi-Rad <kdr@Kians-MacBook-Pro.local>
2017-04-03 14:32:31 -0700

    added cal services, built, it works, no bug

--- CallMe/CallMe/KDR/EventKitFrameWork/Services/ICalServices.h
+++ CallMe/CallMe/KDR/EventKitFrameWork/Services/ICalServices.h
@@ -9,5 +9,28 @@
 #ifndef ICalServices_h
 #define ICalServices_h
 
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <EventKit/EventKit.h>
+#import <EventKitUI/EventKitUI.h>
+
+
+
+@interface ICalServices: NSObject <EKEventEditViewDelegate>
+// EKEventStore instance associated with the current Calendar application
+@property (nonatomic, strong) EKEventStore *eventStore;
+
+// Default calendar associated with the above event store
+@property (nonatomic, strong) EKCalendar *defaultCalendar;
+
+// Array of all events happening within the next 24 hours
+@property (nonatomic, strong) NSMutableArray *eventsList;
+
+// Public Functions
+- (void) loadCalanderServices;
+- (void) checkEventStoreAccessForCalendar;
+- (NSMutableArray *)fetchEvents;
+
+@end
 
 #endif /* ICalServices_h */
