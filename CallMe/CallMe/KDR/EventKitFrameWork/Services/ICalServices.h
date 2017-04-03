6f517baeb2821b05dec4310a92933043ff09ee58
Kian Davoudi-Rad <kdr@Kians-MacBook-Pro.local>
2017-04-03 14:20:22 -0700

    added empty files for EventKit

---
+++ CallMe/CallMe/KDR/EventKitFrameWork/Services/ICalServices.h
@@ -0,0 +1,13 @@
+//
+//  ICalServices.h
+//  CallMe
+//
+//  Created by Kian Davoudi-Rad on 2017-04-03.
+//  Copyright © 2017 Kian Davoudi-Rad. All rights reserved.
+//
+
+#ifndef ICalServices_h
+#define ICalServices_h
+
+
+#endif /* ICalServices_h */
---
+++ CallMe/CallMe/KDR/EventKitFrameWork/Services/ICalServices.m
@@ -0,0 +1,9 @@
+//
+//  ICalServices.m
+//  CallMe
+//
+//  Created by Kian Davoudi-Rad on 2017-04-03.
+//  Copyright © 2017 Kian Davoudi-Rad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
